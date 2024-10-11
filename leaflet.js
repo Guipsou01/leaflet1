8a5cd5ece69d87693e101c13347874f170e46523
Guipsou01 <162042095+Guipsou01@users.noreply.github.com>
2024-10-12 00:27:05 +0200

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -22,3 +22,7 @@ L.imageOverlay(imageUrl2, imageBounds2).addTo(map);
 var imageUrl3 = 'https://static.wikia.nocookie.net/terminalmontage/images/4/4e/Screen_Shot_2022-07-15_at_3.30.11_PM.png',
     imageBounds3 = [[0.05, 0.05], [0.15, 0.15]];
 L.imageOverlay(imageUrl3, imageBounds3).addTo(map);
+//image 4
+var imageUrl3 = 'https://i.pinimg.com/736x/25/bd/8b/25bd8b7f6e57cdfd17747b25d753b2ce.jpg',
+    imageBounds3 = [[-0.05, -0.05], [0.05, 0.05]];
+L.imageOverlay(imageUrl3, imageBounds3).addTo(map);
