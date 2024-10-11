b790c59d53a74c41d776e62cd4be7107a868b781
Guipsou01 <162042095+Guipsou01@users.noreply.github.com>
2024-10-12 00:06:51 +0200

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,13 +1,1 @@
-document.write('-1-');
-//
-var img = new Image();
-img.src = "https://media.tenor.com/ejmDdRGqKDUAAAAe/terminal-montage-donkey-kong.png";
 
-img.onload = function() {
-    var width = img.width;
-    var height = img.height;
-};
-document.write(img.height);
-document.write(img.width);
-document.write('-2-');
-//image 1
