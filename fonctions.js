ebe312f457808ae3300881486c5f82c21593a1eb
Guipsou01 <162042095+Guipsou01@users.noreply.github.com>
2024-11-10 22:42:12 +0200

    Update fonctions.js

--- fonctions.js
+++ fonctions.js
@@ -6,9 +6,9 @@ const unfound_img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4fqK
 var sheetNameFocus;
 var mouseLat = 0;
 var mouseLng = 0;
+var mouseLngLastState = 0;
 var rgImgSelect = 0;
 var customButton;
-var editMode = false;
 var fenetreModale = document.getElementById("fenetreModaleId");
 var btnModal = document.getElementById("btnModalId");
 var croixModal = document.getElementsByClassName("croixModalId")[0];
@@ -23,7 +23,23 @@ var sheetNames = [];//nom des onglets
 var objlistGoogle = [];//liste de tous les objets de la liste google (comprend les fonctions dans leur état le plus simple)
 var objListLeaflet = [];//liste de tout les objets affichable dans leaflet (sans restrictions graphiques) (data, obj, rang, isActif)
 var texteCharg = 0;
+
+const MODE_LECTURE = 0;
+const MODE_DEPLACEMENT = 1;
+const MODE_ROTATION = 2;
+const MODE_ECHELLE = 3;
+const TILEMAP_DEFAULT = 0;
+
+var mode = MODE_LECTURE;
+
+const NULL = 1;
+const MARKER = 2;
+const IMAGE = 3;
+const MARKER_STATIC_MS = 4;
 const leaflet = new LeafletMap();
+
+//MARKER: donnees = cmd,x,y,lx,ly,url,desc
+//IMAGE: donnees = cmd,p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,pRefx,pRefy,url,title,author,website,imageSizeX,imageSizeY,imageLx,imageLy,isMipmap,angle
 
 async function corps(){
   texteCharg = document.getElementById('texteInfo');
@@ -46,7 +62,7 @@ async function corps(){
 }
 async function isShort(data){
   var retour = false;
-  if(data[0] == 'IMG-LX-CENTER' || data[0] == 'IMG-LX-CENTER-R') {
+  if(data[0] == IMAGE) {
     var tailleMini = 0;
     if(data[17] < data[18]) tailleMini = data[17];
     else tailleMini = data[18];
@@ -58,12 +74,15 @@ async function isShort(data){
 async function resetAllMapContent(){
   //desactivation bouton reset
   btnMaps.disabled = true;
+  actionEnCours = ACTNULL;
+  mode = MODE_LECTURE;
+  btnEditorContent.textContent = "Editor (off)";
   //récupération des données de la feuille google visée
   try{
     var donneesGoogleUnOnglet = await getContenuTableauGoogle();
     //console.log("[initialisation] Suppression de tous les objets de la carte...");
     texteCharg.innerHTML = "Suppression de tous les objets de la carte...";
-    leaflet.removeAllObj();
+    leaflet.removeAllObj(true);
     //console.log("[initialisation] Suppression du contenu dess liste Leaflet...");
     texteCharg.innerHTML = "Suppression du contenu des listes Leaflet...";
     objListLeaflet = [];
@@ -101,26 +120,20 @@ async function traitementLigneGoogleSimplifiee(){
       //console.log("traitement nouvelle ligne");
       texteCharg.innerHTML = `Remplissage du contenu des listes Leaflet: ${rang} / ${objlistGoogle.length - 1}`;
         var ligneptee = objlistGoogle[rang];
-      switch(ligneptee[0]){
-        case 'TILEMAP-DEFAULT':
+        if(ligneptee[0] == TILEMAP_DEFAULT){
           objet = await generateTile(ligneptee);
-          break;
-        case 'MARKER':
+        }
+        else if(ligneptee[0] == MARKER){
           objet = await generateMarker(ligneptee);
-          break;
-        case 'IMG-LX-CENTER':
+        }
+        else if(ligneptee[0] == IMAGE){
           objet = await generateImage(ligneptee);
           if(objet != null) objet2 = await generateImage(toMipMap(ligneptee));
-          break;
-        case 'IMG-LX-CENTER-R':
-          objet = await generateImage3p(ligneptee);
-          if(objet != null) objet2 = await generateImage3p(toMipMap(ligneptee));
-          break;
-        default:
+        }
+        else{
           console.error("etat non compris:" + ligneptee);
-          break;
-      }
-      if(objet == null) await objListLeaflet.push([['MARKER',x1,y1,10,10,unfound_img,"Image " + lignes[7] + " not found, check URL"],objet,rang,false]);
+        }
+      if(objet == null) await objListLeaflet.push([[MARKER,x1,y1,10,10,unfound_img,"Image " + lignes[7] + " not found, check URL"],objet,rang,false]);
       if(objet != null) await objListLeaflet.push([ligneptee, objet, rang, false]);
       if(objet2 != null) await objListLeaflet.push([await toMipMap(ligneptee), objet2, rang + 1, false]);
     }
@@ -133,15 +146,15 @@ async function calculTracabilite(rg) {
     var imagePosInScreenVar_ = await objectPosInScreen(lignes);//vérifie si l'objet devrait etre dans l'écran
     var isTooShort_ = await isShort(lignes);//vérifie si l'objet devrait etre affiché petit ou non
     //=====
-    if(lignes[0] == 'TILEMAP-DEFAULT'){
+    if(lignes[0] == TILEMAP_DEFAULT){
       objListLeaflet[rg][3] = true;
     }
     //=====
-    else if(lignes[0] == 'MARKER'){
+    else if(lignes[0] == MARKER){
       objListLeaflet[rg][3] = imagePosInScreenVar_;
     }
     //=====
-    else if(lignes[0] == 'IMG-LX-CENTER' || lignes[0] == 'IMG-LX-CENTER-R'){
+    else if(lignes[0] == IMAGE){
       if(imagePosInScreenVar_ == true && lignes[19] == false) {
         if(isTooShort_ == false) {
           objListLeaflet[rg][3] = true;
@@ -154,7 +167,7 @@ async function calculTracabilite(rg) {
       }
       else if(!imagePosInScreenVar_) objListLeaflet[rg][3] = false;
     }
-    else if(lignes[0] == 'MARKER-STATIC-MS'){
+    else if(lignes[0] == MARKER_STATIC_MS){
       //géré directement par mushroomselector ?
       if(!imagePosInScreenVar_ && objListLeaflet[rg][3] == true) await resetSelector();
     }
@@ -166,16 +179,16 @@ async function calculTracabilite(rg) {
 }
 /**vérifie si un objet similaire éxiste dans l'écran*/
 async function objectPosInScreen(data) {
-  if(data[0] == 'TILEMAP-DEFAULT'){
+  if(data[0] == TILEMAP_DEFAULT){
     return true;
   }
-  else if(data[0] == 'IMG-LX-CENTER' || data[0] == 'IMG-LX-CENTER-R') {
+  else if(data[0] == IMAGE) {
     return (await leaflet.getMapBounds()).intersects([[data[6], data[5]], [data[2], data[1]]]);
   }
-  else if(data[0] == 'MARKER'){
+  else if(data[0] == MARKER){
     return (await leaflet.getMapBounds()).contains([data[2], data[1]]);
   }
-  else if(data[0] == 'MARKER-STATIC-MS'){
+  else if(data[0] == MARKER_STATIC_MS){
     return (await leaflet.getMapBounds()).contains([selectorPosY, selectorPosX])
   }
   return false;
@@ -185,7 +198,7 @@ async function objectPosInScreen(data) {
 async function getLayerObjByData(data){
   var retour = null;
   for(var i = 0; i < objListLeaflet.length; i++){
-    if(isTwoLayerSimilarContent(objListLeaflet[i][0],data,true) == true) retour = objListLeaflet[i][1];
+    if(await isTwoLayerSimilarContent(objListLeaflet[i][0],data,true) == true) retour = objListLeaflet[i][1];
     return retour;
   }
   return retour;
@@ -215,12 +228,12 @@ async function objectRGByObj(obj){
  * (lecture a l'initialisation de map)
 */
 async function traitementLigneGoogle(lignes, rg, nbtot){
-  console.log("traitement nouvelle ligne");
+  //console.log("traitement nouvelle ligne");
   texteCharg.innerHTML = `Remplissage du contenu des liste Google: ${rg} / ${nbtot - 1}`;
   return new Promise((resolve) => {
     switch(lignes[0]){
       case 'TILEMAP-DEFAULT':
-        objlistGoogle.push(lignes);
+        objlistGoogle.push([TILEMAP_DEFAULT]);
         resolve();
       break;
       case 'MARKER':
@@ -232,8 +245,8 @@ async function traitementLigneGoogle(lignes, rg, nbtot){
           imagePtee.src = lignes[4];
           imagePtee.onload = function() {
             var ly = imagePtee.height / imagePtee.width * lx;
-            //donnees = cmd,x,y,lx,ly,url,desc
-            objlistGoogle.push(['MARKER',convertToFloat(lignes[1]),convertToFloat(lignes[2]),lx,ly,lignes[4],imgDesc]);
+            //MARKER: donnees = cmd,x,y,lx,ly,url,desc
+            objlistGoogle.push([MARKER,convertToFloat(lignes[1]),convertToFloat(lignes[2]),lx,ly,lignes[4],imgDesc]);
             resolve();
           }
         }());
@@ -250,13 +263,13 @@ async function traitementLigneGoogle(lignes, rg, nbtot){
           //console.log(lignes[7]);
           imagePtee.onload = function() {
             ly = imagePtee.height / imagePtee.width * lx;
-            //donnees = cmd,p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,pRefx,pRefy,url,title,author,website,imageSizeX,imageSizeY,imageLx,imageLy,isMipmap
-            objlistGoogle.push(['IMG-LX-CENTER',x1 - lx/2,y1 - ly/2,x1 + lx/2,y1 - ly/2,x1 + lx / 2,y1 + ly / 2,x1 - lx/2,y1 + ly/2,x1,y1,lignes[6],lignes[7],lignes[8],lignes[9], imagePtee.width, imagePtee.height, lx, ly,false]);
+            //IMAGE: donnees = cmd,p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,pRefx,pRefy,url,title,author,website,imageSizeX,imageSizeY,imageLx,imageLy,isMipmap,angle
+            objlistGoogle.push([IMAGE,x1 - lx/2,y1 - ly/2,x1 + lx/2,y1 - ly/2,x1 + lx / 2,y1 + ly / 2,x1 - lx/2,y1 + ly/2,x1,y1,lignes[6],lignes[7],lignes[8],lignes[9], imagePtee.width, imagePtee.height, lx, ly,false,null]);
             resolve();
           };
           imagePtee.onerror = function() {
             //console.log("image non chargee: " + lignes[7]);
-            objlistGoogle.push(['MARKER',x1,y1,10,10,unfound_img,"Image " + lignes[7] + " not found, check URL"]);
+            objlistGoogle.push([MARKER,x1,y1,10,10,unfound_img,"Image " + lignes[7] + " not found, check URL"]);
             resolve();
           };
         }());
@@ -272,40 +285,19 @@ async function traitementLigneGoogle(lignes, rg, nbtot){
           //console.trace("angle: " + angle + " " + lignes[7])
           var ly = convertToFloat(0);
           imagePtee.src = lignes[6];
-          imagePtee.onload = function() {
+          imagePtee.onload = async function() {
             //angle = 0;
             ly = imagePtee.height / imagePtee.width * lx;
-            //image((x - lx/2),(y - ly / 2),(x + lx / 2),(y + ly / 2),imgSize.src);
-            //calcul des nouvelles positions des trois coins BD, HD, HG depuis la valeur d'angle
-            var posAx = lx * Math.cos(degToRad(angle));
-            var posAy = lx * Math.sin(degToRad(angle));
-            var posBx = ly * Math.cos(degToRad(angle + 90));
-            var posBy = ly * Math.sin(degToRad(angle + 90));
-            //application des nouvelles positions (seul x1 et y1 ne bouge pas)
-            var x2P = x1 + posAx;
-            var y2P = y1 + posAy;
-            var x3P = x1 + posAx + posBx;
-            var y3P = y1 + posAy + posBy;
-            var x4P = x1 + posBx;
-            var y4P = y1 + posBy;
-            //modification de l'image par rapport au mode de placement de point de base
-            var x1F = x1 - (average(x1,x2P,x3P,x4P) - x1);
-            var y1F = y1 - (average(y1,y2P,y3P,y4P) - y1);
-            var x2F = x2P - (average(x1,x2P,x3P,x4P) - x1);
-            var y2F = y2P - (average(y1,y2P,y3P,y4P) - y1);
-            var x3F = x3P - (average(x1,x2P,x3P,x4P) - x1);
-            var y3F = y3P - (average(y1,y2P,y3P,y4P) - y1);
-            var x4F = x4P - (average(x1,x2P,x3P,x4P) - x1);
-            var y4F = y4P - (average(y1,y2P,y3P,y4P) - y1);
+            var tabl = await getPosApresRotation(x1,y1,lx,ly,angle);
             //donnees = cmd,p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,pRefx,pRefy,url,title,author,website,imageSizeX,imageSizeY,imageLx,imageLy,isMipmap,angle
-            objlistGoogle.push(['IMG-LX-CENTER-R',x1F,y1F,x2F,y2F,x3F,y3F,x4F,y4F,x1,y1,lignes[6],lignes[7],lignes[8],lignes[9], imagePtee.width, imagePtee.height, lx, ly, false, angle]);
+            objlistGoogle.push([IMAGE,tabl[0],tabl[1],tabl[2],tabl[3],tabl[4],tabl[5],tabl[6],tabl[7],tabl[8],tabl[9],lignes[6],lignes[7],lignes[8],lignes[9], imagePtee.width, imagePtee.height, lx, ly, false, angle]);
             //console.log("retour ok");
             resolve();
           }
           imagePtee.onerror = function() {
             // Si l'image n'est pas valide
             //console.log("image non chargee: " + lignes[7]);
-            objlistGoogle.push(['MARKER',x1,y1,10,10,unfound_img,"Image " + lignes[7] + " not found, check URL"]);
+            objlistGoogle.push([MARKER,x1,y1,10,10,unfound_img,"Image " + lignes[7] + " not found, check URL"]);
             resolve();
           };
         }());
@@ -321,7 +313,7 @@ async function traitementLigneGoogle(lignes, rg, nbtot){
           imagePtee.onload = async function() {
             ly = imagePtee.height / imagePtee.width * lx;
             ly = ly;
-            objlistGoogle.push(['IMG-LX',x,y,x + lx,y + ly,imagePtee.src,""]);
+            objlistGoogle.push([IMAGE,x,y,x + lx,y + ly,imagePtee.src,""]);
             resolve();
           }
         }());
@@ -339,7 +331,7 @@ async function traitementLigneGoogle(lignes, rg, nbtot){
         var x3modif = ly * Math.cos(degToRad(angle + 90));
         var y3modif = ly * Math.sin(degToRad(angle + 90));
         //
-        objlistGoogle.push(['IMG-PLR',x1 + x3modif + x2modif,y1 + y3modif + y2modif,x1 + x3modif,y1 + y3modif,x1 + x2modif,y1 + y2modif,"https://media.tenor.com/ejmDdRGqKDUAAAAe/terminal-montage-donkey-kong.png",0]);
+        objlistGoogle.push([IMAGE,x1 + x3modif + x2modif,y1 + y3modif + y2modif,x1 + x3modif,y1 + y3modif,x1 + x2modif,y1 + y2modif,"https://media.tenor.com/ejmDdRGqKDUAAAAe/terminal-montage-donkey-kong.png",0]);
         resolve();
         //
       break;
@@ -349,17 +341,71 @@ async function traitementLigneGoogle(lignes, rg, nbtot){
     }
   });
 }
-//fenetre modale
+/**retourne un tableau de 10 val représentant les 5 points de l'image: p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,pRefx,pRefy depuis le point central de l'image, ces proportions et son angle final*/
+async function getPosApresRotation(x1, y1, lx, ly, angle){
+  //calcul des nouvelles positions des trois coins BD, HD, HG depuis la valeur d'angle
+  var posAx = lx * Math.cos(degToRad(angle));
+  var posAy = lx * Math.sin(degToRad(angle));
+  var posBx = ly * Math.cos(degToRad(angle + 90));
+  var posBy = ly * Math.sin(degToRad(angle + 90));
+  //application des nouvelles positions (seul x1 et y1 ne bouge pas)
+  var x2P = posAx;
+  var y2P = posAy;
+  var x3P = posAx + posBx;
+  var y3P = posAy + posBy;
+  var x4P = posBx;
+  var y4P = posBy;
+  //
+  x2P += x1;
+  y2P += y1;
+  x3P += x1;
+  y3P += y1;
+  x4P += x1;
+  y4P += y1;
+  //modification de l'image par rapport au mode de placement de point de base
+  var x1F = x1 - (average(x1,x2P,x3P,x4P) - x1);
+  var y1F = y1 - (average(y1,y2P,y3P,y4P) - y1);
+  var x2F = x2P - (average(x1,x2P,x3P,x4P) - x1);
+  var y2F = y2P - (average(y1,y2P,y3P,y4P) - y1);
+  var x3F = x3P - (average(x1,x2P,x3P,x4P) - x1);
+  var y3F = y3P - (average(y1,y2P,y3P,y4P) - y1);
+  var x4F = x4P - (average(x1,x2P,x3P,x4P) - x1);
+  var y4F = y4P - (average(y1,y2P,y3P,y4P) - y1);
+  return [x1F, y1F, x2F, y2F, x3F, y3F, x4F, y4F, x1, y1];
+}
+/**appui sur le bouton credits */
 btnModal.onclick = function() {
     fenetreModale.style.display = "block";
 }
+/**appui sur la croix de la fenetre modale */
 croixModal.onclick = function() {
     fenetreModale.style.display = "none";
 }
+/**appui sur la fenetre */
 window.onclick = function(event) {
     if (event.target == fenetreModale) {
         fenetreModale.style.display = "none";
     }
+}
+/**appui sur le bouton editeur */
+btnEditor.onclick = function() {
+  if(mode == MODE_LECTURE) {
+    mode = MODE_DEPLACEMENT;
+    btnEditorContent.textContent = "Editor (move)";
+  }
+  else if(mode == MODE_DEPLACEMENT) {
+    mode = MODE_ROTATION;
+    btnEditorContent.textContent = "Editor (rotation)";
+  }
+  else if(mode == MODE_ROTATION) {
+    mode = MODE_ECHELLE;
+    btnEditorContent.textContent = "Editor (scale)";
+  }
+  else if(mode == MODE_ECHELLE) {
+    mode = MODE_LECTURE;
+    btnEditorContent.textContent = "Editor (off)";
+  }
+  actionImageFocus();
 }
 //fonctions diverses
 function degToRad(degrees) {
@@ -368,18 +414,6 @@ function degToRad(degrees) {
 function average(a, b, c, d){
   return ((a + b + c + d) / 4);
 };
-btnEditor.onclick = function() {
-  /*if(editMode == true) {
-    editMode = false;
-    updateSelector();
-    btnEditorContent.textContent = "Editor (off)";
-  }
-  else if(editMode == false) {
-    editMode = true;
-    updateSelector();
-    btnEditorContent.textContent = "Editor (on)";
-  }*/
-}
 /**récupere le contenu complet de l'onglet Google focus*/
 async function getContenuTableauGoogle() {
   try {
@@ -416,18 +450,18 @@ async function createSelector(){
     link.href = "#";
     link.textContent = option;
     link.onclick = function(event) {
-      event.preventDefault(); // Empêche le comportement par défaut du lien
-      btnMaps.textContent = option; // Mettre à jour le texte du bouton
-      btnMapsList.style.display = 'none'; // Masquer la liste après sélection
+      event.preventDefault(); //Empêche le comportement par défaut du lien
+      btnMaps.textContent = option; //Mettre à jour le texte du bouton
+      btnMapsList.style.display = 'none'; //Masquer la liste après sélection
       sheetNameFocus = option;
       resetAllMapContent();
     };
-    btnMapsList.appendChild(link); // Ajouter le lien à la liste
+    btnMapsList.appendChild(link); //Ajouter le lien à la liste
   });
   btnMaps.onclick = function() {
     btnMapsList.style.display = btnMapsList.style.display === 'none' ? 'block' : 'none';
   };
-  // Fermer la liste déroulante quand on clique en dehors
+  //Fermer la liste déroulante quand on clique en dehors
   window.onclick = function(event) {
     if (!event.target.matches('#btnMaps')) {
       btnMapsList.style.display = 'none';
@@ -442,18 +476,14 @@ function isTwoLayerSimilarContent(layer1, layer2, isAbsoluteTest){
   else if(layer1.length == 0 || layer2.length == 0){
     throw new Error("objet vide: [" + layer1 + " ===== " + layer2 + "]");
   }
-  else if(layer1[0] == 'IMG-LX-CENTER' && layer2[0] == 'IMG-LX-CENTER'){
+  else if(layer1[0] == IMAGE && layer2[0] == IMAGE){
     if(isAbsoluteTest) return (layer1[0] == layer2[0] && layer1[12] == layer2[12] && layer1[19] == layer2[19]);
     else return (layer1[0] == layer2[0] && layer1[12] == layer2[12]);
   }
-  else if(layer1[0] == 'IMG-LX-CENTER-R' && layer2[0] == 'IMG-LX-CENTER-R'){
-    if(isAbsoluteTest) return (layer1[0] == layer2[0] && layer1[12] == layer2[12] && layer1[19] == layer2[19]);
-    else return (layer1[0] == layer2[0] && layer1[12] == layer2[12]);
-  }
-  else if(layer1[0] == 'MARKER' && layer2[0] == 'MARKER'){
+  else if(layer1[0] == MARKER && layer2[0] == MARKER){
     return (layer1[0] == layer2[0] && layer1[6] == layer2[6]);
   }
-  else if(layer1[0] == 'MARKER-STATIC-MS' && layer2[0] == 'MARKER-STATIC-MS'){
+  else if(layer1[0] == MARKER_STATIC_MS && layer2[0] == MARKER_STATIC_MS){
     return true;
   }
   else return false;
