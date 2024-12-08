7009f2d38530a7cb41391194e14c7c00aa9ce2ef
Guipsou01 <162042095+Guipsou01@users.noreply.github.com>
2024-12-08 19:58:16 +0200

    Update and rename vector.js to vecteur.js

--- vecteur.js
+++ vecteur.js
@@ -3,7 +3,8 @@ class V2F{
     #x = 0;
     #y = 0;
     #po = null;//another V2F reference as origin
-    pe = [];//V2F enfants
+    #data = null;//objet de référence lié au vecteur, facultatif, sert pour retrouver un objet depuis le réseau de vecteur
+    #pe = [];//V2F enfants
     constructor(x = 0,y = 0){
         this.#x = x,
         this.#y = y;
@@ -19,6 +20,9 @@ class V2F{
     /**récupère l'adresse du point d'origine. Si inexistant, retourne 0 [Pt]*/
     get po(){
         return this.getPo();
+    }
+    get pe(){
+        return this.#pe;
     }
     /**récupère l'adresse du point d'origine. Si inexistant, retourne une erreur [Pt]*/
     getPo(){
@@ -118,9 +122,9 @@ class V2F{
         this.x = Math.cos(degToRad(a));
         this.y = Math.sin(degToRad(a));
     }
-    /**retourne l'angle du vecteur en degrés */
+    /**retourne l'angle du vecteur en degrés, retourne 0 si vecteur nul*/
     getAngle(){
-        if (this.x == 0 && this.y == 0)  throw new Error("Vecteur nul");
+        if (this.x == 0 && this.y == 0)  return 0;
         return (Math.atan2(this.y, this.x) * 180 / Math.PI + 360) % 360;
     }
     /**applique une rotation depuis un vecteur éxistant en gardant la distance originale*/
@@ -146,12 +150,52 @@ class V2F{
     }
     /**applique un décalage de rotation uniquement sur parents*/
     applyRotationDecalageOnEnfants(v2f){
-        for(var i = 0; i < this.pe.length; i++){
-            this.pe[i].applyRotationDecalage(v2f);
-            this.pe[i].applyRotationDecalageOnEnfants(v2f);
+        for(var i = 0; i < this.#pe.length; i++){
+            this.#pe[i].applyRotationDecalage(v2f);
+            this.#pe[i].applyRotationDecalageOnEnfants(v2f);
         }
     }
+    /**applique un décalage de rotation uniquement sur parents
+     * val: valeur strictement positive avec équilibre a 1
+    */
+    applyScaleDecalageOnEnfants(val){
+        for(var i = 0; i < this.#pe.length; i++){
+            this.#pe[i].applyScaleDecalage(val);
+            this.#pe[i].applyScaleDecalageOnEnfants(val);
+        }
+    }
+    /**applique un décalage de rotation depuis un vecteur éxistant en gardant la distance originale
+     * val: valeur strictement positive avec équilibre a 1
+    */
+    applyScaleDecalage(val){
+        if(val == null) throw new Error("v2f nul");
+        if (isNaN(val)) throw new Error("Le paramètre doit être une instance de V2F.");
+        //enregistre la distance actuelle
+        if(val <= 0) new ErreurIO("la valeur doit etre strictement positive avec comme centre d'équilibre 1: " + val);
+        //
+        this.x *= val;
+        this.y *= val;
+    }
     nbParents(){
-        return this.pe.length;
+        return this.#pe.length;
+    }
+    setData(val){
+        this.#data = val;
+    }
+    /*retourne les objets de chaque vecteur sous forme de liste*/
+    getData(){
+        var liste = [];
+        liste.push(this.#data);
+        for(var i = 0; i < this.#pe.length; i++){
+            if(liste.length > 1000000) throw new Error("impossible de lire une famille de plus de 1000000 éléments");
+            if(this.#pe[i] != null) {
+                liste.push(...this.#pe[i].getData());
+            }
+        }
+        if(liste.length == 0){
+            console.log("liste vide");
+            return null;
+        }
+        else return liste;
     }
 }
