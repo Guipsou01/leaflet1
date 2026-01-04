34bc3575c4b77b41523848939bed1562c523ffff
Guipsou01 <162042095+Guipsou01@users.noreply.github.com>
2026-01-04 12:47:25 +0100

    Update modale.js

--- modale.js
+++ modale.js
@@ -5,17 +5,19 @@ class FenetreModale {
     #fenetreCredits = null;
     #isActif = false;
     constructor(modaleContent, croixCredits, fenetreCreditsID){
-        this.#popupContent = modaleContent;
-        this.#croixCredits = croixCredits;
-        this.#fenetreCredits = fenetreCreditsID;
-        /**appui sur la croix de la fenetre modale */
-        this.#croixCredits.onclick = () => {
-            this.close();
+        try{
+            if(!modaleContent) throw new Error("modaleContent non initialisé");
+            if(!croixCredits) throw new Error("croixCredits non initialisé");
+            if(!fenetreCreditsID) throw new Error("fenetreCreditsID non initialisé");
+            this.#popupContent = modaleContent;
+            this.#croixCredits = croixCredits;
+            this.#fenetreCredits = fenetreCreditsID;
+            /**appui sur la croix de la fenetre modale */
+            this.#croixCredits.onclick = () => {this.close();}
+            /**appui sur la fenetre */
+            window.onclick = (event) => {if(event.target === this.#fenetreCredits) this.close();}
         }
-        /**appui sur la fenetre */
-        window.onclick = (event) => {
-            if(event.target === this.#fenetreCredits) this.close();
-        }
+        catch (error) {console.error("Error:", error);}
     }
     openWithContent(texte){
         if(this.#popupContent == null) throw new Error("fenetre modale non init");
